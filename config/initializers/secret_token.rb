18b5eec009a99bc1825a77b48adbdc49833f1c33
Hieu <hieu@hieu-VirtualBox.(none)>
2012-08-18 23:56:32 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbTest::Application.config.secret_token = '193ec35e596f630d0d7330e345ed8a3886bdfd497056df0e97e13f1ec8ae71b0c8d021c6aafeb7250f45ab17592446a003b76e7e2aad286f2656bff4e4597d89'
