5115e1488e7977723bc5315982300df3824dc44f
Hieu <hieu@hieu-VirtualBox.(none)>
2012-08-25 09:33:42 +0700

    Get post by different way

--- app.rb
+++ app.rb
@@ -66,7 +66,7 @@ get "/" do
     @friends = @graph.get_connections('me', 'friends')
     @photos  = @graph.get_connections('me', 'photos')
     @likes   = @graph.get_connections('me', 'likes').first(4)
-    @post    = @user.stream[:posts] if @user.stream?
+    @post    = @graph.search(nil, {:type => "post"})
 
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
