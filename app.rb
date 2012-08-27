01ffc318caaf99c0363a84851cb415b1c7f62c0f
Hieu <hieu@hieu-VirtualBox.(none)>
2012-08-27 22:06:29 +0700

    Get post by different way

--- app.rb
+++ app.rb
@@ -63,11 +63,8 @@ get "/" do
 
   if session[:access_token]
     @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
-    #@post    = @graph.search(nil, {:type => "post"})
     @post    = @graph.get_connections("me", "posts")
+    @permission = @graph.get_connections("me", "permission")
 
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
